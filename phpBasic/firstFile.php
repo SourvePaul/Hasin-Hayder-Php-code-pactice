b3b1a0477595e8eedeb4a352dce5158694ab033b
SourvePaul <you@example.com>
2022-11-08 22:20:43 +0600

    4rd commit

--- phpBasic/firstFile.php
+++ phpBasic/firstFile.php
@@ -12,9 +12,23 @@
 // printf("%s is living on %s", strtoupper($name), strrev($mean));
 // // two time '' use kortea holea '\' use kortea hobea
 
-$n = 12;
-$o = 012;
-$h = 0x1B;
-printf("The number is %d %d and %d \n",$n, $o, $h);
 
-printf("The number of Binary is %d to %b", $n, $n);
+
+// //Octal, Decimal, HexaDecimal Number System
+// $n = 12;
+// $o = 012;
+// $h = 0x1B;
+// printf("The number is %d %d and %d \n",$n, $o, $h);
+
+// printf("The number of Binary is %d to %b \n", $n, $n);
+
+// printf("The number of HexaDecimal is %d to %x \n", $h, $h);
+
+// printf("The number of Octal is %d to %o \n", $o, $o);
+
+
+
+//Anything move first or last
+$fName = "Faysal";
+$lName = "Ahmed";
+printf('His Name is %2$s %1$s', $lName, $fName);
