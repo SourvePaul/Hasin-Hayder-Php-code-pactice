762e3562b6adee9deaabb5b42315e04274cb04af
SourvePaul <you@example.com>
2022-11-08 22:06:11 +0600

    3rd commit

--- phpBasic/firstFile.php
+++ phpBasic/firstFile.php
@@ -1,14 +1,20 @@
 <?php
-$name = "Saimun Hossen";
-var_dump($name);
+// $name = "Saimun Hossen";
+// var_dump($name);
 
-$result= true;
-var_dump($result);
+// $result= true;
+// var_dump($result);
 
-$mean = "Earth";
-echo 'We\'are living on {$mean}'."\n";
-echo "We're living on ".$mean. "\n";
-printf(" We're living on %s\n", $mean);
-printf("%s is living on %s", strtoupper($name), strrev($mean));
-// two time '' use kortea holea '\' use kortea hobea
+// $mean = "Earth";
+// echo 'We\'are living on {$mean}'."\n";
+// echo "We're living on ".$mean. "\n";
+// printf(" We're living on %s\n", $mean);
+// printf("%s is living on %s", strtoupper($name), strrev($mean));
+// // two time '' use kortea holea '\' use kortea hobea
 
+$n = 12;
+$o = 012;
+$h = 0x1B;
+printf("The number is %d %d and %d \n",$n, $o, $h);
+
+printf("The number of Binary is %d to %b", $n, $n);
