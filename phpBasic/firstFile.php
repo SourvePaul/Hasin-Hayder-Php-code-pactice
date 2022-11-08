bc5a6ebbe0df758ec32f03978d96943355f8ff3c
SourvePaul <you@example.com>
2022-11-08 21:48:27 +0600

    first basic commit

---
+++ phpBasic/firstFile.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Saimun Hossen";
+var_dump($name);
+
+$result= true;
+var_dump($result);
+
+$mean = "Earth";
+echo 'We\'are living on {$mean}'."\n";
+echo "We're living on ".$mean. "\n";
+printf(" We're living on %s\n", $mean);
+printf("%s is living on %s", strtoupper($name), strrev($mean));
+// two time '' use kortea holea '\' use kortea hobea
+?>
