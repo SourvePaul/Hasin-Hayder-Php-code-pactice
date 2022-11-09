7bc0cc396d24dd95fbbfd7f723c96d25690c7591
SourvePaul <you@example.com>
2022-11-09 21:16:49 +0600

    short commit

--- phpBasic/hardConditionShortCut.php
+++ phpBasic/hardConditionShortCut.php
@@ -15,3 +15,47 @@ if($year %4 == 0 && $year %100 == 0 && $year %400 == 0 ) {
 }else {
     echo "{$year} is't a Leap Year number. ";
 }
+
+echo "\n";
+//other way
+
+if($year %4 == 0 && ($year %100 != 0 || $year %400 == 0)) {
+    echo "{$year} is a Leap Year"; 
+}else {
+    echo "{$year} is't a Leap Year";
+}
+
+echo "\n";
+
+//nested if
+$num = true;
+$num2 = false;
+$num3 = true;
+
+if($num) {
+    if($num2) {
+        if($num3) {
+            echo "Hello";
+        }else{
+            echo "No1";
+        }
+    }else{
+        echo "No2";
+    }
+}else{
+    echo "No3";
+}
+
+echo "\n";
+
+//shortcut
+
+if($num && $num2 && $num3) {
+    echo "Hello";
+}elseif($num && $num2) {
+    echo "NO1";
+}elseif($num){
+    echo "No2";
+}else {
+    echo "No3";
+}
