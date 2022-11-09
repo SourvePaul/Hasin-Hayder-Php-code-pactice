59e7d7184cc125259a02faa8da9a86f5ce420748
SourvePaul <you@example.com>
2022-11-09 20:28:47 +0600

    new commit

---
+++ phpBasic/logicalOperator.php
@@ -0,0 +1,18 @@
+<?php
+$age = 13;
+ if( $age >= 13 && $age==13) {
+    echo "This person not a older";
+ }
+else{
+    echo "This person is a teen ager";
+}
+
+echo "\n";
+//Different way to diclear "=="
+
+$sFood = "Tuna";
+if( "Tuna" == $sFood) {
+    echo "This is a Sea Food";
+}else{
+    echo "This is't a sea food";
+}
