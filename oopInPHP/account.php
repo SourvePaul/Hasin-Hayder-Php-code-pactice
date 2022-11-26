5c0d6d359a5978fdf2ccc4e5d916725805e3410d
SourvePaul <you@example.com>
2022-11-26 21:18:34 +0600

    oop in php

---
+++ oopInPHP/account.php
@@ -0,0 +1,11 @@
+<?php
+
+require "inheritance.php";
+
+class Account extends Inreitance {
+
+}
+
+$inheritance = new Inreitance();
+
+echo 'Blance is : '.$inheritance->bank();
---
+++ oopInPHP/inheritance.php
@@ -0,0 +1,11 @@
+<?php
+class Inreitance {
+    public function bank() {
+        return 500;
+    }
+
+    public function deposite() {
+        return 1000;
+    }
+    
+}
