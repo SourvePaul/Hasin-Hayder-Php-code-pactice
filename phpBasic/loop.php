99ad3d240b42287c4fbf2476672da5f2ed0b60ac
SourvePaul <you@example.com>
2022-11-27 20:24:10 +0600

    loop

--- phpBasic/loop.php
+++ phpBasic/loop.php
@@ -4,3 +4,15 @@ for( $i=1; $i<=10; $i++){
     echo $i;
     echo PHP_EOL;
 }
+
+$i=0;
+while($i<9){
+    $i++;
+    echo $i.PHP_EOL;
+}
+
+$i=11;
+do{
+    $i++;
+    echo $i.PHP_EOL;
+}while($i<20);
